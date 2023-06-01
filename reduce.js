8dc13b638cc97b7b9dd8ef9c626e3129067e4b54
mristimaki <rosanna.ristimaki@gmail.com>
2023-06-01 02:44:47 +0000

    Initial commit

--- reduce.js
+++ reduce.js
@@ -5,7 +5,24 @@
  */
 
 
+// First we have created extended code (commented out) just
+// to show how it actually works. Then how to use the simpler version.
+
+// const nums = [1, 2, 3, 4];
+// let sum = nums.reduce((acc, curr) => {
+  // console.log(
+    // "Accumulator:", acc,
+    // "Current value:", curr,
+    // "Total:", acc + curr
+  // );
+  // return acc + curr;
+// });
+// console.log(sum);
+
 // Summing an array of numbers:
+const nums = [1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr);
+console.log(sum);
 
 
 const teamMembers = [
